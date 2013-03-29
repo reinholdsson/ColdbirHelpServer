fce355d6c6ac02c26af8a6d10c54729b4b2c3e87
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-03-29 18:45:33 +0100

    Add custom css

--- inst/global.R
+++ inst/global.R
@@ -4,8 +4,20 @@ require(rHighcharts)
 db <- cdb(.help_args$path, type = "f")
 
 # Fetch all variables
-list_vars <- vars(db)
-list_vars <- setNames(as.list(list_vars), list_vars)
+v <- vars(db)
+
+col <- "title"  # title name in documentation
+v_names <- sapply(v, function(x) {
+    title <- tryCatch(get_doc(db, x)[[col]], error = function(e) NULL)
+    if (is.null(title)) {
+        title <- x 
+    } else {
+        title <- paste(title, " (", x, ")", sep = "")
+    }
+    return(title)
+})
+
+v <- setNames(as.list(v), v_names)
 
 #sel_choices <- expand.grid(LETTERS,letters)
 #sel_choices <- paste(sel_choices[,1],sel_choices[,2], sep="-")
--- inst/ui.R
+++ inst/ui.R
@@ -1,23 +1,37 @@
 
+# Custom CSS
+custom_css <- function(title, window_title = title){
+    tagList(
+        tags$head(
+            tags$title(window_title),
+            tags$link(rel="stylesheet", type="text/css",
+                      href="style.css"),
+            tags$style(type="text/css", '#variable { width:500px; }')
+        )
+    )
+}
+
 # Interface
 shinyUI(bootstrapPage(
-    #headerPanel('Select2shiny'),
+
+    # Include javascript files
+    includeHTML("www/js/tools.js"),
     
-    #sidebarPanel(
-        includeHTML("www/js/tools.js"),
+    # CSS styling
+    custom_css("Coldbir Help Server"),
+
+    # Input
+    selectInput("variable", label = "Select variable:", choices = v, selected = NULL, multiple = FALSE),
         
-        div(class="row",
-            div(class="span3",
-                selectInput("variable", label = "Select variable:", choices = list_vars, selected = NULL, multiple = FALSE)
-            ),
-            div(class="span6", 
-                htmlOutput("docs")
-                #htmlOutput("freq_chart")
-            ),
-            div(class="span5",
-                plotOutput("charts", height = "500px"),
-                #htmlOutput("summary")
-                htmlOutput("table")
-            )
+    div(class="row",
+        div(class="span6", 
+            htmlOutput("docs")
+            #htmlOutput("freq_chart")
+        ),
+        div(class="span5",
+            plotOutput("charts", height = "500px"),
+            #htmlOutput("summary")
+            htmlOutput("table")
         )
+    )
 ))
